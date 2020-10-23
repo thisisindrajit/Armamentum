ccf55fc34cdd95d16934c5bd423064a3caf878a9
Indrajit <indrajitvijayakumar@gmail.com>
2020-10-23 10:05:16 +0530

    Updates

---
+++ precache-manifest.1ca557c84c098a19ca46f820fb50ad22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c023359bd755bd2e279bfa0f3622af",
+    "url": "/Armamentum/index.html"
+  },
+  {
+    "revision": "9b252f1ac073551d128a",
+    "url": "/Armamentum/static/css/main.6628449a.chunk.css"
+  },
+  {
+    "revision": "b4e31a72a6912a8030ad",
+    "url": "/Armamentum/static/js/2.341521ec.chunk.js"
+  },
+  {
+    "revision": "b34d611d69419bc1205d1e94f9169aa7",
+    "url": "/Armamentum/static/js/2.341521ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b252f1ac073551d128a",
+    "url": "/Armamentum/static/js/main.9b9fdbc6.chunk.js"
+  },
+  {
+    "revision": "221205674402b1621a15",
+    "url": "/Armamentum/static/js/runtime-main.bcaa7ecd.js"
+  },
+  {
+    "revision": "0d338a54a4d2948145e78b236c559fb8",
+    "url": "/Armamentum/static/media/logo.0d338a54.png"
+  }
+]);
