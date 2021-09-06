f5dd78962b733c40f3d4a08503b8eb3b88753a46
Indrajit <indrajitvijayakumar@gmail.com>
2021-09-06 12:00:07 +0530

    migrated weather api

--- src/Body/Weather/Weather.js
+++ src/Body/Weather/Weather.js
@@ -5,67 +5,71 @@ export default class Weather extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      weather: {
-                  temp: {
-                    value: undefined,
-                  }
-                },
+      weather: null,
       isLoading: true,
-      url:"",
-      enableLocation:false
+      url: "",
+      enableLocation: false,
+      error: false
     };
   }
-  
+
   async componentDidMount() {
-   
+
     if (navigator.geolocation) {
-      await navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({url:"https://armamentum.herokuapp.com/weather/" + position.coords.latitude + "&" + position.coords.longitude, enableLocation:true});
+      navigator.geolocation.getCurrentPosition((position) => {
+        // this.setState({url:"https://armamentum.herokuapp.com/weather/" + position.coords.latitude + "&" + position.coords.longitude, enableLocation:true});
+        // });
+        this.setState({ url: "http://localhost:5000/weather/" + position.coords.latitude + "&" + position.coords.longitude, enableLocation: true });
       });
     }
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
 
-    if(this.state.url !== "" && this.state.isLoading === true){
+    if (this.state.url !== "" && this.state.isLoading === true) {
 
-    fetch(this.state.url)
-      .then((res) => res.json())
-      .then((data) => {
-        //console.log(data);
+      fetch(this.state.url)
+        .then((res) => res.json())
+        .then((res) => {
+          //console.log(res);
 
-        if (data.message === "API rate limit exceeded") {
-          this.setState({
-            weather: {
-              temp: {
-                value: undefined,
-              },
-            },
-            isLoading: false,
-          });
-        } else {
-          this.setState({ weather: data, isLoading: false });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        this.setState({ isLoading: false });
-      });
+          // if (data.message === "API rate limit exceeded") {
+
+          //   this.setState({
+          //     weather: {
+          //       temp: {
+          //         value: undefined,
+          //       },
+          //     },
+          //     isLoading: false,
+          //   });
+          // }
+
+          if (res.data.timelines) {
+            this.setState({ weather: res.data, isLoading: false });
+          } else {
+            this.setState({ isLoading: false, error: true });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          this.setState({ isLoading: false, error: true });
+        });
     }
   }
 
   render() {
     return this.state.isLoading === false &&
-      this.state.weather.temp.value !== undefined ? (
+      this.state.weather !== null ? (
       <div id="weather">
         <div id="city">Current temperature in your location</div>
         <span id="temperature">
-          {Math.round(this.state.weather.temp.value) + "°C"}
+          {Math.round(this.state.weather.timelines[0].intervals[0].values.temperature) + "°C"}
         </span>
       </div>
     ) : (
       <div id="weather">
-        <span>{this.state.isLoading === true && this.state.enableLocation === true ? "Loading..." : this.state.enableLocation === false ? "Please provide location access!" : "Sorry! Some error occurred! Please try again!"}</span>
+        <span>{this.state.error === false ? (this.state.isLoading === true && this.state.enableLocation === true ? "Loading..." : "Please provide location access!") : "Sorry! Some error occurred! Please try again!"}</span>
       </div>
     );
   }
